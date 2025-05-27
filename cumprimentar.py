c6c67c91aa711a339af86cfd88865526fced0a7e
Nyannyk <kookminstick@gmail.com>
2025-05-27 08:13:39 -0400

    Add files via upload

---
+++ cumprimentar.py
@@ -0,0 +1,9 @@
+def cumprimentar_pessoa(nome):
+    print(f"Olá, {nome}! Tudo bem com você?")
+    
+cumprimentar_pessoa("Maria")
+cumprimentar_pessoa("Jonas")
+cumprimentar_pessoa("Johnny")
+cumprimentar_pessoa("Erik")
+    
+    
---
+++ funcao_com_mais_argumento.py
@@ -0,0 +1,7 @@
+def apresentar_pet(nome_animal, tipo_animal):
+    print(f"Este é o {nome_animal}!, ele é um {tipo_animal} muito esperto")
+    
+apresentar_pet("Rex", "cachorro")
+apresentar_pet("Miau","gato")
+apresentar_pet("AUur", "Lobo")
+    
